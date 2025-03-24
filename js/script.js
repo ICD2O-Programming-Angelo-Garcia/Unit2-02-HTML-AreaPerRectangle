c3690c35e34d0f7f07ec5d443ca57d4cd9a82dc7
Angelo Garcia <angelo.garcia@stu.ocsb.ca>
2025-03-24 14:00:40 +0000

    Sentence about what you did

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Angelo Garcia All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Angelo Garcia
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
+
+function doMathClicked () {
+  // this function does basic math
+  document.getElementById('multiply-math').innerHTML = '5 x 3 = ' + '(5 x 3)
+  document.getElementById('subtract-math').innerHTML = '7 - 3 =  ' + (7 - 3)
+  document.getElementById('multiply-math').innerHTML = '3 + 4 * 2 = ' + (3 + 4 * 2)
+  document.getElementById('divide-math').innerHTML = '(4 / 2) + 3 = ' + ((4 / 2) + 3)
+  document.getElementById('exponent-math').innerHTML = '5 + 2 ** 3 = ' + (5 + 2 ** 3)
+	document.getElementById('square-root-math').innerHTML = '√(81) = ' + Math.sqrt(81)
+}
+
+     
