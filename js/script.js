367934e8cc044b912d7b0fc6da6b4ecf139d2861
Angelo Garcia <angelo.garcia@stu.ocsb.ca>
2025-03-25 14:14:03 +0000

    Sentence about what you did

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Angelo Garcia All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Angelo Garcia
+// Created on: march 2025
 // This file contains the JS functions for index.html
+
+function enterClicked () {
+  // this function gets user's first name and age and displays it back to user
+
+  // get first name from text field
+  let firstName = document.getElementById("first-name").value
+	// get age from text field and cast it to integer
+  let userAge = parseInt(document.getElementById("age-entered").value)
+
+  // display name and age back to user
+  document.getElementById('user-info').innerHTML = "Your first name is " + firstName + " and you are " + userAge + " years old."
+}
