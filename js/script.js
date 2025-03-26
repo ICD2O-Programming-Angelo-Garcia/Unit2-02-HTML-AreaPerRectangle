05b25bcdb1958663a885f7557856d6cd528bfe40
Angelo Garcia <angelo.garcia@stu.ocsb.ca>
2025-03-26 14:59:04 +0000

    updated user info

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 // Copyright (c) 2025 Angelo Garcia All rights reserved
 //
 // Created by: Angelo Garcia
-<<<<<<< HEAD
 // Created on: march 2025
 // This file contains the JS functions for index.html
 
@@ -16,19 +15,3 @@ function enterClicked () {
   // display name and age back to user
   document.getElementById('user-info').innerHTML = "Your first name is " + firstName + " and you are " + userAge + " years old."
 }
-=======
-// Created on: Mar 2025
-// This file contains the JS functions for index.html
-
-function doMathClicked () {
-  // this function does basic math
-  document.getElementById('multiply-math').innerHTML = '5 x 3 = ' + '(5 x 3)
-  document.getElementById('subtract-math').innerHTML = '7 - 3 =  ' + (7 - 3)
-  document.getElementById('multiply-math').innerHTML = '3 + 4 * 2 = ' + (3 + 4 * 2)
-  document.getElementById('divide-math').innerHTML = '(4 / 2) + 3 = ' + ((4 / 2) + 3)
-  document.getElementById('exponent-math').innerHTML = '5 + 2 ** 3 = ' + (5 + 2 ** 3)
-	document.getElementById('square-root-math').innerHTML = '√(81) = ' + Math.sqrt(81)
-}
-
-     
->>>>>>> c3690c35e34d0f7f07ec5d443ca57d4cd9a82dc7
